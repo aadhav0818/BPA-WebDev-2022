aac91b3c2d390807ff937226c30168ae17896a03
Kavinraj23 <kavin.raj@outlook.com>
2022-12-22 04:12:11 -0600

    made changes to car finder and navbar

--- scripts/car-finder.js
+++ scripts/car-finder.js
@@ -50,21 +50,29 @@ mileageInput.addEventListener("keypress", function(event) {
 // add a specific class to the buttons that makes the button darker so that the user knows they selected it
 var preferredStyles = [];
 
-function selectVehicle(vehicleType) {
+function selectVehicle(vehicleType, indx) {
+    var btn = document.querySelectorAll(".body-btn")[indx];
     if(!preferredStyles.includes(vehicleType)) {
+        btn.classList.remove("btn-outline-dark");
+        btn.classList.add("btn-light");
         preferredStyles.push(vehicleType);
     } else {
+        btn.classList.remove("btn-light");
+        btn.classList.add("btn-outline-dark");
         preferredStyles.splice(preferredStyles.indexOf(vehicleType), 1);
     }
 }
 
 
-
 function findCars() {
-    document.querySelector(".car-quiz").remove();
     let maxPrice = priceInput.value;
     let maxMiles = mileageInput.value;
     let bodyStyles = preferredStyles;
+    if(maxPrice == null || maxMiles == null || bodyStyles.length == 0) {
+        alert("Make sure that all the fields are filled out properly!");
+        return;
+    }
+    document.querySelector(".car-quiz").remove();
     let matchedIndices = []
     for(var i = 0; i < vehicle_data.length; i++) {
         if(evalMiles(maxMiles, vehicle_data[i].miles) && evalPrice(maxPrice, vehicle_data[i].price) && evalBodyStyles(bodyStyles, vehicle_data[i].type)) {
@@ -86,13 +94,13 @@ function displayMatchedVehicles(indices) {
         let checkpoint = vehicle_data[index].brand.replace("Mercedes-Benz", "Mercedes")
         let rel_filepath = vehicle_data[index].year + "-" + vehicle_data[index].color + "-" + checkpoint + "-" +  rel_make;
         
-        document.querySelector(".car-results") +=
+        document.querySelector(".car-results").innerHTML +=
         `
         <div class="col-sm-12 col-md-6 col-lg-3 " style="padding-top: 3rem;"> 
             <div class="card bg-dark text-white id-${vehicle_data[index].id}" style="width: 100%;">
                 <img src="./vehicle-images/${rel_filepath}.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">${vehicle_data[index].year} ${vehicle_data[index].color} ${vehicle_data[i].brand} ${vehicle_data[index].model}</h5>
+                    <h5 class="card-title">${vehicle_data[index].year} ${vehicle_data[index].color} ${vehicle_data[index].brand} ${vehicle_data[index].model}</h5>
                     <p class="card-text text-white" style="margin-bottom: 0px;">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-currency-exchange" viewBox="0 0 16 16">
                         <path d="M0 5a5.002 5.002 0 0 0 4.027 4.905 6.46 6.46 0 0 1 .544-2.073C3.695 7.536 3.132 6.864 3 5.91h-.5v-.426h.466V5.05c0-.046 0-.093.004-.135H2.5v-.427h.511C3.236 3.24 4.213 2.5 5.681 2.5c.316 0 .59.031.819.085v.733a3.46 3.46 0 0 0-.815-.082c-.919 0-1.538.466-1.734 1.252h1.917v.427h-1.98c-.003.046-.003.097-.003.147v.422h1.983v.427H3.93c.118.602.468 1.03 1.005 1.229a6.5 6.5 0 0 1 4.97-3.113A5.002 5.002 0 0 0 0 5zm16 5.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0zm-7.75 1.322c.069.835.746 1.485 1.964 1.562V14h.54v-.62c1.259-.086 1.996-.74 1.996-1.69 0-.865-.563-1.31-1.57-1.54l-.426-.1V8.374c.54.06.884.347.966.745h.948c-.07-.804-.779-1.433-1.914-1.502V7h-.54v.629c-1.076.103-1.808.732-1.808 1.622 0 .787.544 1.288 1.45 1.493l.358.085v1.78c-.554-.08-.92-.376-1.003-.787H8.25zm1.96-1.895c-.532-.12-.82-.364-.82-.732 0-.41.311-.719.824-.809v1.54h-.005zm.622 1.044c.645.145.943.38.943.796 0 .474-.37.8-1.02.86v-1.674l.077.018z"/>
@@ -105,14 +113,15 @@ function displayMatchedVehicles(indices) {
                     </svg>  
                     Distance: ${vehicle_data[index].miles} mi
                     </p>
-                    <a href="#" class="btn btn-outline-success btn-inventory-card">$ | Purchase</a>
+                    <div style='text-align: center;'>
+                        <a href="./contact-us.html" class="btn btn-outline-secondary btn-inventory-card"><i class="fa-solid fa-user"></i> | Check Availability</a>
+                    </div>   
                 </div>
             </div>
         </div>
         `
     }
 }
-
 
 let vehicle_data = 
 [

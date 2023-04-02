12dcbecf6136f4d5e0cb93631d01779d3cdfbf74
arcturus <aadhav0818@gmail.com>
2023-04-01 22:30:24 -0500

    scroll animations boxes

--- scripts/page-loader.js
+++ scripts/page-loader.js
@@ -2,7 +2,10 @@ if(sessionStorage.getItem("deleteLoader") != null)
 {
     console.log("NUL")
     const element = document.getElementById("page-loader-body");
+    const element2 = document.getElementById("page-loader-body");
+
     element.remove(); 
+    element2.remove();
     const main_page = document.getElementById("homepage");
     main_page.style.display = "Inline";
 }

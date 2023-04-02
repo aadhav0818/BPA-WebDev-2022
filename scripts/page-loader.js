648dfd77de72997608ec83f8beb6d8e2dd91b905
arcturus <aadhav0818@gmail.com>
2023-04-01 19:37:53 -0500

    page loader

--- scripts/page-loader.js
+++ scripts/page-loader.js
@@ -1,8 +1,19 @@
 addEventListener('load', function(event)
 {
-    const doc = document.getElementById("page-loader-body");
-    const main_page = document.getElementById("homepage");
+    
+    setTimeout(() => {
+        const doc = document.getElementById("page-loader-body");
+        const main_page = document.getElementById("homepage");
+        
+        doc.classList.add('fadeouthome');
+        setTimeout(() => {
+            doc.style.display = "None";
+        }, "1000");
+        setTimeout(() => {
+            main_page.style.display = "Inline";
+        }, "1000");
+    }, "1000");
 
-    doc.style.display = "None";
-    main_page.style.display = "Inline";
+ 
+
 })

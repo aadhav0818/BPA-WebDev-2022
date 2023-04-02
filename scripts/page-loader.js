a1d1c49952a92c2debf6c3f0568aa93567e060ed
arcturus <aadhav0818@gmail.com>
2023-04-01 20:31:38 -0500

    page loader

--- scripts/page-loader.js
+++ scripts/page-loader.js
@@ -1,6 +1,33 @@
-addEventListener('load', function(event)
+
+if(sessionStorage.getItem("visited" != null))
 {
+
+    if(sessionStorage.getItem("visited" == false))
+    {
+        let element = document.getElementById("loaderimage");
+        element.classList.add("loader-logo")
+    }
     
+}
+else
+{
+    sessionStorage.setItem("visited", false)
+    console.log(sessionStorage.getItem("visited"))
+    const element = document.getElementById("loaderimage");
+    console.log(element)
+    element.classList.add('loader-logo')
+
+    document.onreadystatechange = () => {
+        if (document.readyState === 'complete') {
+          transitionJS();
+        }
+    };
+    sessionStorage.setItem("visited", true)
+
+}
+
+function transitionJS()
+{
     setTimeout(() => {
         const doc = document.getElementById("page-loader-body");
         const main_page = document.getElementById("homepage");
@@ -12,8 +39,8 @@ addEventListener('load', function(event)
         setTimeout(() => {
             main_page.style.display = "Inline";
         }, "1000");
+
+
+        main_page.style.backgroundColor = "#353333";
     }, "1000");
-
- 
-
-})
+}
